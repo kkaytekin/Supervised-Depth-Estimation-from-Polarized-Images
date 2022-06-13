c192ab7a300beee89e842ff6a4f5b50395e7ee19
ge39gol <p.ruhkamp@tum.de>
2022-06-13 18:26:08 +0200

    Modify imports

--- manydepth/networks/__init__.py
+++ manydepth/networks/__init__.py
@@ -3,11 +3,3 @@ from .resnet_encoder import ResnetEncoder, ResnetEncoderMatching
 from .depth_decoder import DepthDecoder
 from .pose_decoder import PoseDecoder
 from .pose_cnn import PoseCNN
-from .ddv_decoder import MSDepthDecoder
-from .resnet_encoder_attention import ResnetEncoderMatchingAttention
-from .epipolar import Epipolar
-from .resnet_encoder_MAD import ResnetEncoderMatchingMAD
-from .unet import UNet
-from .image_conv import ImageConv
-from .gwcnet import GwcNet_G, GwcNet_GC
-from .loss import model_loss
