ed3c1a85ac3a38400a82b6454d07b9f988e265cf
ge65mak <witold.pacholarz@tum.de>
2022-07-07 19:16:52 +0200

    Let's make it work again

--- train_supervised_GT.sh
+++ train_supervised_GT.sh
@@ -3,7 +3,7 @@
 python3 -m manydepth.train --png --num_depth_bins 96  --num_workers 8 --batch_size 12 \
   --data_path /media/jungo/Research/Datasets/HAMMER/train/ \
   --data_path_val /media/jungo/Research/Datasets/HAMMER/test_unseen/ \
-  --log_dir /media/jungo/Research/Experiments/AT3DCV/ \
+  --log_dir /media/jungo/Research/Experiments/AT3DCV/EXPERIMENTS \
   --num_epochs 50 --scheduler_step_size 15 --freeze_teacher_epoch 50 --learning_rate 1e-4 \
   --width 480 --height 320 \
   --dataset HAMMER --split HAMMER --eval_split HAMMER_unseen \
@@ -15,8 +15,8 @@ python3 -m manydepth.train --png --num_depth_bins 96  --num_workers 8 --batch_si
   --modality polarization \
   --depth_modality _gt \
   --model_name Letsmakeitfinallywork_RGB \
-  --augment_xolp \
-  --augment_normals \
+#  --augment_xolp \
+#  --augment_normals \
 #  --overfit True
 
 #python3 -m manydepth.train --png --num_depth_bins 96  --num_workers 8 --batch_size 12 \
